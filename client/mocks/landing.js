80454969fe2bf60a137793b03e3d3d0f407b5a2b
Nicholas Cheung <nicholas.cheung@flex.com>
2018-01-11 01:10:17 -0800

    [Extension] Rename screen model "landing" to "menu"

--- client/mocks/landing.js
+++
@@ -1,3 +0,0 @@
-'use strict';
-window.mocks = window.mocks || {};
-window.mocks.landing = window.mocks.landing || {};
---
+++ client/mocks/menu.js
@@ -0,0 +1,3 @@
+'use strict';
+window.mocks = window.mocks || {};
+window.mocks.menu = window.mocks.menu || {};
