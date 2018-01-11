9a58a428522dee905bda30a73ff3d73e4f6f3ec9
Nicholas Cheung <nicholas.cheung@flex.com>
2018-01-11 03:01:41 -0800

    [Studio] Delete screen "app.searchcontrol"

--- client/controllers/app/searchcontrol.js
+++
@@ -1,5 +0,0 @@
-angular.module('app').controller('app_searchcontrol', app_searchcontrol);
-function app_searchcontrol($scope, app) {
-    'use strict';
-    app.init($scope);
-}
--- client/states.js
+++ client/states.js
@@ -51,14 +51,5 @@ angular.module('app').config(function ($stateProvider) {
                 }
             }
         }
-    }).state('app.searchcontrol', {
-        views: {
-            app: {
-                controller: 'app_searchcontrol',
-                templateProvider: function (app) {
-                    return app.templateProvider('app.searchcontrol');
-                }
-            }
-        }
     });
 });
