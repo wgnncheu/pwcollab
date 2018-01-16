4304ea45890e2775ba7f6e91050e36d530707273
Nicholas Cheung <nicholas.cheung@flex.com>
2018-01-16 03:53:44 -0800

    [Studio] Add screen "orderviewsearchcontrols"

--- client/states.js
+++ client/states.js
@@ -69,5 +69,14 @@ angular.module('app').config(function ($stateProvider) {
                 }
             }
         }
+    }).state('app.orderviewsearchcontrols', {
+        views: {
+            app: {
+                controller: 'app_orderviewsearchcontrols',
+                templateProvider: function (app) {
+                    return app.templateProvider('app.orderviewsearchcontrols');
+                }
+            }
+        }
     });
 });
