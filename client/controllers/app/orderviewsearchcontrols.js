66d30218b036c882e3506cab060aeebe37ced1ef
Nicholas Cheung <nicholas.cheung@flex.com>
2018-01-16 04:07:55 -0800

    [Studio] Add screen "orderviewsearchcontrols"

---
+++ client/controllers/app/orderviewsearchcontrols.js
@@ -0,0 +1,5 @@
+angular.module('app').controller('app_orderviewsearchcontrols', app_orderviewsearchcontrols);
+function app_orderviewsearchcontrols($scope, app) {
+    'use strict';
+    app.init($scope);
+}
